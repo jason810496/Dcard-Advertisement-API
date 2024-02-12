f68bb398adc1480c0d6067eaca8a2ffd726c9759
jason810496 <810496@email.wlsh.tyc.edu.tw>
2024-02-12 23:56:14 +0800

    refactor: improve readability of error handling

--- pkg/utils/error.go
+++ pkg/utils/error.go
@@ -1,18 +1,50 @@
 package utils
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
 
 // NewError example
 func NewError(ctx *gin.Context, status int, err error) {
-	er := HTTPError{
-		Code:    status,
-		Message: err.Error(),
+	var validate_errors validator.ValidationErrors
+
+	if errors.As(err, &validate_errors) {
+		out := make([]ErrorMsg, len(validate_errors))
+		for i, err := range validate_errors {
+			out[i] = ErrorMsg{
+				Field:   err.Field(),
+				Message: getErrorMsg(err),
+			}
+		}
+
+		ctx.AbortWithStatusJSON(status, HTTPError{
+			Code:   status,
+			Errors: out,
+		})
 	}
-	ctx.JSON(status, er)
+}
+
+func getErrorMsg(fe validator.FieldError) string {
+	switch fe.Tag() {
+	case "required":
+		return "This field is required"
+	case "gte", "gtefield":
+		return "Should be greater than " + fe.Param()
+	case "oneof":
+		return "Should be one of " + fe.Param()
+	}
+	return "Unknown error"
+}
+
+type ErrorMsg struct {
+	Field   string `json:"field"`
+	Message string `json:"message" example:"bad request"`
 }
 
 // HTTPError example
 type HTTPError struct {
-	Code    int    `json:"code" example:"400"`
-	Message string `json:"message" example:"status bad request"`
+	Code   int        `json:"code" example:"400"`
+	Errors []ErrorMsg `json:"errors"`
 }
