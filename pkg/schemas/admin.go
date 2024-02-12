689958a9426f0f740a2c592863769dd04a5c373c
jason810496 <810496@email.wlsh.tyc.edu.tw>
2024-02-12 23:54:46 +0800

    fix: add validation for request schemas - fix validation for `CreateAdRequest`     - using `time.Time` instead of `string` for `date` field     - introduce `CreateAdConditions`  to represent the conditions field - add validation for `Public API`     - add `PublicAdRequest` schemas binding and validation

--- pkg/schemas/admin.go
+++ pkg/schemas/admin.go
@@ -1,15 +1,19 @@
 package schemas
 
+import "time"
+
+type CreateAdConditions struct {
+	AgeStart int      `json:"ageStart" binding:"required,numeric,min=1,max=100" example:"18"`
+	AgeEnd   int      `json:"ageEnd" binding:"required,numeric,min=1,max=100,gtefield=AgeStart" example:"30"`
+	Country  []string `json:"country" binding:"required,dive,oneof=TW HK JP US KR" example:"TW,JP"`
+	Platform []string `json:"platform" binding:"required,dive,oneof=ios android web" example:"ios,android"`
+}
+
 type CreateAdRequest struct {
-	Title      string `json:"title" binding:"required" example:"AD 123"`
-	StartAt    string `json:"startAt" binding:"required" example:"2024-01-01 10:00:00"`
-	EndAt      string `json:"endAt" binding:"required" example:"2024-02-01 10:00:00"`
-	Conditions []struct {
-		AgeStart int      `json:"ageStart" binding:"required" example:"18"`
-		AgeEnd   int      `json:"ageEnd" binding:"required" example:"30"`
-		Country  []string `json:"country" binding:"required" example:"TW,JP,KR,US"`
-		Platform []string `json:"platform" binding:"required" example:"ios,web"`
-	} `json:"conditions" binding:"required"`
+	Title      string             `json:"title" binding:"required" example:"AD 123"`
+	StartAt    time.Time          `json:"startAt" binding:"required" example:"2023-12-10T03:00:00.000Z"`
+	EndAt      time.Time          `json:"endAt" binding:"required,gtefield=StartAt" example:"2023-12-31T16:00:00.000Z"`
+	Conditions CreateAdConditions `json:"conditions" binding:"required"`
 }
 
 type CreateAdResponse struct {
--- pkg/schemas/public.go
+++ pkg/schemas/public.go
@@ -8,3 +8,19 @@ type PublicAdItem struct {
 type PublicAdResponse struct {
 	Items []PublicAdItem `json:"items"`
 }
+
+type PublicAdRequest struct {
+	Age      int    `form:"age" binding:"omitempty,numeric,min=1,max=100" example:"18"`
+	Country  string `form:"country" binding:"omitempty,oneof=TW HK JP US KR" example:"TW"`
+	Platform string `form:"platform" binding:"omitempty,oneof=ios android web" example:"ios"`
+	Gender   string `form:"gender" binding:"omitempty,oneof=F M" example:"F"`
+	Limit    int    `form:"limit" binding:"omitempty,numeric,min=1" example:"10"`
+	Offset   int    `form:"offset" binding:"omitempty,numeric,min=0" example:"0"`
+}
+
+func NewPublicAdRequest() PublicAdRequest {
+	instance := PublicAdRequest{}
+	instance.Limit = 100
+	instance.Offset = 0
+	return instance
+}
