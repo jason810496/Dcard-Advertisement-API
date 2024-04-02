c364c769a35235b8be4a9edb40be2772b562c30e
jason810496 <810496@email.wlsh.tyc.edu.tw>
2024-04-02 11:08:29 +0800

    refactor: sitmulate real user behavior

--- k6/load-test.js
+++ k6/load-test.js
@@ -9,7 +9,7 @@ export const options = {
 			executor: 'constant-arrival-rate',
 			rate: 20000,
 			timeUnit: '1s',
-			duration: '5m',
+			duration: '30s',
 			preAllocatedVUs: 1000,
 			maxVUs: 4000,
 		},
@@ -25,40 +25,58 @@ export const options = {
 let urlString = `http://${__ENV.API_HOST}:${__ENV.API_PORT}/api/v1/ad`;
 
 const limits = [10, 20, 30, 40, 50];
+const offsets = [0, 10, 20, 30, 40, 50];
 const genders = ["F", "M"];
-const countries = ["TW", "HK", "JP", "US", "KR"];
-const platforms = ["ios", "android", "web"];
+const countries = ["HK", "JP", "US", "KR"]; // TW
+const platforms = ["ios", "android"]; // "web"
 
 export default function () {
 	const limit = randomItem(limits);
-	let age = -1;
+	const offset = randomItem(offsets);
 	const gender = randomItem(genders);
-	const country = randomItem(countries);
-	const platform = randomItem(platforms);
 
-	if (exec.vu.idInTest % 10 < 1) {
-		// 10%
-		age = randomIntBetween(1, parseInt(__ENV.AGE_END));
+	const countryRand = randomIntBetween(1, 10);
+	const platformRand = randomIntBetween(1, 10);
+
+	let age = -1;
+	let country;
+	let platform;
+
+	// simulate real-world traffic age distribution
+	if (exec.vu.idInTest % 10 < 1) { // 10%
+		age = randomIntBetween(1, parseInt(__ENV.AGE_END)-1);
 	}
-	else if (exec.vu.idInTest % 10 < 8) {
-		// 80%
+	else if (exec.vu.idInTest % 10 < 9) { // 80%
 		age = randomIntBetween(parseInt(__ENV.AGE_START), parseInt(__ENV.AGE_END));
 	}
-	else{
-		// 10%
-		age = randomIntBetween(parseInt(__ENV.AGE_START), 100);
+	else{ // 10%
+		age = randomIntBetween(parseInt(__ENV.AGE_START)+1, 100);
 	}
 
-	for (let i = 0; i < 10; i++) {
-		http.get(
-			`${urlString}?limit=${limit}&offset=${i}&age=${age}&gender=${gender}&country=${country}&platform=${platform}`,
-			{
-				tags: {
-					name: 'GetAd'
-				}
-			}
-		);
+	// simulate real-world traffic country distribution
+	if (countryRand < 8) { // 80%
+		country = "TW";
 	}
+	else { // 20%
+		country = randomItem(countries);
+	}
+
+	// simulate real-world traffic platform distribution
+	if (platformRand < 8) { // 80%
+		platform = randomItem(platforms);
+	}
+	else { // 20%
+		platform = "web";
+	}
+
+	http.get(
+		`${urlString}?limit=${limit}&offset=${offset}&age=${age}&gender=${gender}&country=${country}&platform=${platform}`,
+		{
+			tags: {
+				name: 'GetAd'
+			}
+		}
+	);
 
 	sleep(1);
 }
