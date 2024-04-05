6827dba8b51cade6c8d01a4b6519f125142e6fea
jason810496 <810496@email.wlsh.tyc.edu.tw>
2024-04-05 23:47:53 +0800

    fix: fix correct time format for RFC3339 with Microseconds

--- pkg/utils/constant.go
+++ pkg/utils/constant.go
@@ -3,3 +3,4 @@ package utils
 var GenderList = []string{"F", "M"}
 var CountryList = []string{"TW", "HK", "JP", "US", "KR"}
 var PlatformList = []string{"ios", "android", "web"}
+var RFC3339Custom = "2006-01-02T15:04:05.999999Z07:00"
