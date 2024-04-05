68102a3170c8b982050d9693c7b0dbdf6c3cf7e4
jason810496 <810496@email.wlsh.tyc.edu.tw>
2024-04-05 22:01:53 +0800

    refactor: add graceful shutdown with bg scheduler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
 	"github.com/jason810496/Dcard-Advertisement-API/pkg/cache"
 	"github.com/jason810496/Dcard-Advertisement-API/pkg/config"
 	"github.com/jason810496/Dcard-Advertisement-API/pkg/database"
@@ -13,13 +21,60 @@ import (
 )
 
 func main() {
+	// Create context that listens for the interrupt signal from the OS.
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	// init all component
+	Init()
+	// setup Router
+	router := InitRouter()
+	apiSrv := &http.Server{
+		Addr:    config.Settings.App.Address,
+		Handler: router,
+	}
+	//  API goroutine
+	go func() {
+		if err := apiSrv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	// renew localCache goroutine
+	go func() {
+		for {
+			scheduler.RefreshLocalCache(cache.LocalCacheInstance,config.Settings.LocalCache.Interval)
+			time.Sleep(config.Settings.LocalCache.Period)
+		}
+	}()
+
+	// Listen for the interrupt signal.
+	<-ctx.Done()
+	// Restore default behavior on the interrupt signal and notify user of shutdown.
+	stop()
+	log.Println("shutting down gracefully, press Ctrl+C again to force")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := apiSrv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
+}
+
+func Init() {
 	config.Init()
 	database.Init()
+	database.CheckConnection()
 	cache.Init()
 	cache.RedisClientInstance.CheckConnection()
 	cache.LocalCacheInit()
-	scheduler.PreHeatLocalCache(cache.LocalCacheInstance)
+	scheduler.RefreshLocalCache(cache.LocalCacheInstance)
+}
 
+func InitRouter() *gin.Engine {
 	router := handlers.SetupRouter()
 	docs.SwaggerInfo.Title = "Dcard Advertisement API"
 	docs.SwaggerInfo.Version = "1.0"
@@ -27,10 +82,6 @@ func main() {
 	docs.SwaggerInfo.Host = config.Settings.App.Address
 	docs.SwaggerInfo.BasePath = ""
 	docs.SwaggerInfo.Schemes = []string{"http", "https"}
-
-	database.CheckConnection()
-
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	router.Run(config.Settings.App.Address)
-
+	return router
 }
